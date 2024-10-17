495b4e0f9f423ec00883956b4a0d4378aaf24a7d
francescofalcone24 <cicciofalcon97@gmail.com>
2024-10-17 21:21:46 +0300

    url changed

--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -25,7 +25,7 @@ export default {
     console.log(this.$route.params.slug);
 
     axios
-      .get(`http://127.0.0.1:8000/api/projects/${this.$route.params.slug}`)
+      .get(`https://admin.francescowebdev.com/api/projects/${this.$route.params.slug}`)
       .then(response => {
         if (response.data.status) {
           store.singleProject = response.data.progetto;
